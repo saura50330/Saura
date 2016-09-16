7b98d97ec6feac949de7cb84562a3923a055eaaa
saura50330 <nan1ban@yahoo.com>
2016-09-16 19:25:55 +0530

    updated  pressuresensor and other services
    updated  pressuresensor and other services

--- 2_Psoc/Platform/Design02.cydsn/imu_i2c.c
+++ 2_Psoc/Platform/Design02.cydsn/imu_i2c.c
@@ -106,9 +106,9 @@ void MPU9250_Init(void) // should be called when os is running 50 ms task
 								  st_MCU=CALIB; 
 								}	
 								break;
-	    case IMU_CALIB_READ: if(I2C_READ(GYRO_ADDRESS, Gyro_Calib_data[0],6))  // read all gyro values in Gyro_Calib_data 
+	    case IMU_CALIB_READ: if(I2C_READ(GYRO_ADDRESS, Gyro_Read_data[0],6))  // read all gyro values in Gyro_Calib_data 
 						   {
-						    st_IMU=IMU_CALIB_CALC;
+						         st_IMU=IMU_CALIB_CALC;
 					       }
 						   break;
 		case IMU_CALIB_CALC: 
@@ -156,10 +156,125 @@ void MPU9250_Init(void) // should be called when os is running 50 ms task
 						st_IMU=IMU_INIT;
 						// TODO: turn off this task     
 						break;
+	   case IMU_READ_ALL:
+	   
+						float MotionVal[9];
+			
+						MPU9250_READ_ACCEL();
+						MPU9250_READ_GYRO();
+						MPU9250_READ_MAG();
+	   break;
+	   case IMU_CALC_ALL: 
+						MotionVal[0]=gyro[0]/32.8;
+						MotionVal[1]=gyro[1]/32.8;
+						MotionVal[2]=gyro[2]/32.8;
+						MotionVal[3]=accel[0];
+						MotionVal[4]=accel[1];
+						MotionVal[5]=accel[2];
+						MotionVal[6]=magn[0];
+						MotionVal[7]=magn[1];
+						MotionVal[8]=magn[2];
+						
+						IMU_AHRSupdate((float)MotionVal[0] * 0.0175, (float)MotionVal[1] * 0.0175, (float)MotionVal[2] * 0.0175,
+						(float)MotionVal[3], (float)MotionVal[4], (float)MotionVal[5], (float)MotionVal[6], (float)MotionVal[7], MotionVal[8]);
+						
+						// calculate angles
+						Angles[1] = asin(-2 * q1 * q3 + 2 * q0* q2)* 57.3; // pitch
+						Angles[2] = atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3; // roll
+						Angles[0] = atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3;   
+	   break;
+	
 	}
 					       
 }
 
+#define Kp 4.50f   // proportional gain governs rate of convergence to accelerometer/magnetometer
+#define Ki 1.0f    // integral gain governs rate of convergence of gyroscope biases
+
+/**
+  * @brief  Updata attitude and heading 
+  * @param  ax: accelerometer X
+  * @param  ay: accelerometer Y
+  * @param  az: accelerometer Z
+  * @param  gx: gyroscopes X
+  * @param  gy: gyroscopes Y
+  * @param  gz: gyroscopes Z
+  * @param  mx: magnetometer X
+  * @param  my: magnetometer Y
+  * @param  mz: magnetometer Z
+  * @retval None
+  */
+void IMU_AHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz) 
+{
+	float norm;
+	float hx, hy, hz, bx, bz;
+	float vx, vy, vz, wx, wy, wz;
+	float exInt = 0.0, eyInt = 0.0, ezInt = 0.0;
+	float ex, ey, ez, halfT = 0.024f;
+
+	float q0q0 = q0 * q0;
+	float q0q1 = q0 * q1;
+	float q0q2 = q0 * q2;
+	float q0q3 = q0 * q3;
+	float q1q1 = q1 * q1;
+	float q1q2 = q1 * q2;
+	float q1q3 = q1 * q3;
+	float q2q2 = q2 * q2;   
+	float q2q3 = q2 * q3;
+	float q3q3 = q3 * q3;          
+
+	norm = invSqrt(ax * ax + ay * ay + az * az);       
+	ax = ax * norm;
+	ay = ay * norm;
+	az = az * norm;
+
+	norm = invSqrt(mx * mx + my * my + mz * mz);          
+	mx = mx * norm;
+	my = my * norm;
+	mz = mz * norm;
+
+	// compute reference direction of flux
+	hx = 2 * mx * (0.5f - q2q2 - q3q3) + 2 * my * (q1q2 - q0q3) + 2 * mz * (q1q3 + q0q2);
+	hy = 2 * mx * (q1q2 + q0q3) + 2 * my * (0.5f - q1q1 - q3q3) + 2 * mz * (q2q3 - q0q1);
+	hz = 2 * mx * (q1q3 - q0q2) + 2 * my * (q2q3 + q0q1) + 2 * mz * (0.5f - q1q1 - q2q2);         
+	bx = sqrt((hx * hx) + (hy * hy));
+	bz = hz;     
+
+	// estimated direction of gravity and flux (v and w)
+	vx = 2 * (q1q3 - q0q2);
+	vy = 2 * (q0q1 + q2q3);
+	vz = q0q0 - q1q1 - q2q2 + q3q3;
+	wx = 2 * bx * (0.5 - q2q2 - q3q3) + 2 * bz * (q1q3 - q0q2);
+	wy = 2 * bx * (q1q2 - q0q3) + 2 * bz * (q0q1 + q2q3);
+	wz = 2 * bx * (q0q2 + q1q3) + 2 * bz * (0.5 - q1q1 - q2q2);  
+
+	// error is sum of cross product between reference direction of fields and direction measured by sensors
+	ex = (ay * vz - az * vy) + (my * wz - mz * wy);
+	ey = (az * vx - ax * vz) + (mz * wx - mx * wz);
+	ez = (ax * vy - ay * vx) + (mx * wy - my * wx);
+
+	if(ex != 0.0f && ey != 0.0f && ez != 0.0f)
+	{
+		exInt = exInt + ex * Ki * halfT;
+		eyInt = eyInt + ey * Ki * halfT;	
+		ezInt = ezInt + ez * Ki * halfT;
+
+		gx = gx + Kp * ex + exInt;
+		gy = gy + Kp * ey + eyInt;
+		gz = gz + Kp * ez + ezInt;
+	}
+
+	q0 = q0 + (-q1 * gx - q2 * gy - q3 * gz) * halfT;
+	q1 = q1 + (q0 * gx + q2 * gz - q3 * gy) * halfT;
+	q2 = q2 + (q0 * gy - q1 * gz + q3 * gx) * halfT;
+	q3 = q3 + (q0 * gz + q1 * gy - q2 * gx) * halfT;  
+
+	norm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+	q0 = q0 * norm;
+	q1 = q1 * norm;
+	q2 = q2 * norm;
+	q3 = q3 * norm;
+}
 
 
 /**
@@ -174,7 +289,7 @@ void MPU9250_READ_MAG(void)
 	 static int32_t OutBuffer[3] = {0};
 	 static MPU9250_AvgTypeDef MPU9250_Filter[3];
 
-    I2C_WriteOneByte(GYRO_ADDRESS,0x37,0x02);//turn on Bypass Mode 
+    I2C_WriteOneByte(GYRO_ADDRESS,0x37,0x02);//turn on Bypass Mode  
     Delay_ms(10);
     I2C_WriteOneByte(MAG_ADDRESS,0x0A,0x01);	
     Delay_ms(10);
@@ -446,5 +561,125 @@ uint8 I2C_MasterWriteBuf(uint8 slaveAddress, uint8 * wrData, uint8 cnt, uint8 mo
     
 
 // read from slave
+
+void Read_From_ALL(void)
+{
+			switch(st_Read)
+			{
+				case READ_GYRO: 
+					if(I2C_READ(GYRO_ADDRESS, Gyro_Read_Data[0],6))  // read all gyro values in Gyro_Read_Data 
+					{
+							st_Read=CALC_GYRO;
+					}
+					break;
+				case CALC_GYRO: 
+					if(stI2CMgr==IDLE)	             
+					   {
+							InBuffer[0]=(Gyro_Read_Data[1]<<8)|Gyro_Read_Data[0];
+							InBuffer[1]=(Gyro_Read_Data[3]<<8)|Gyro_Read_Data[2];
+							InBuffer[2]=(Gyro_Read_Data[5]<<8)|Gyro_Read_Data[4];
+					
+							for(i = 0; i < 3; i ++)	
+							{
+								MPU9250_CalAvgValue(&MPU9250_Filter[i].Index, MPU9250_Filter[i].AvgBuffer, InBuffer[i], OutBuffer + i);
+							}
+							gyro[0] = *(OutBuffer + 0) - MPU9250_Offset.X;
+							gyro[1] = *(OutBuffer + 1) - MPU9250_Offset.Y;
+							gyro[2] = *(OutBuffer + 2) - MPU9250_Offset.Z;
+							st_Read=READ_ACC;
+						}
+					 break;
+				case READ_ACC: 
+					if(I2C_READ(ACCEL_ADDRESS, Acc_Read_Data[0],6))  // read all gyro values in Acc_Read_Data 
+					{
+							st_Gyro=CALC_ACC;
+					}
+					break;
+				case CALC_ACC: 
+					if(stI2CMgr==IDLE)	             
+					   {
+							InBuffer[0]=(Acc_Read_Data[1]<<8)|Acc_Read_Data[0];
+							InBuffer[1]=(Acc_Read_Data[3]<<8)|Acc_Read_Data[2];
+							InBuffer[2]=(Acc_Read_Data[5]<<8)|Acc_Read_Data[4];
+					
+							for(i = 0; i < 3; i ++)	
+						   {
+							  MPU9250_CalAvgValue(&MPU9250_Filter[i].Index, MPU9250_Filter[i].AvgBuffer, InBuffer[i], OutBuffer + i);
+						   }
+						   accel[0] = *(OutBuffer + 0);
+						   accel[1] = *(OutBuffer + 1);
+						   accel[2] = *(OutBuffer + 2); 
+						   st_Read=BYPASS_GYRO;
+						}
+					  break;
+				case BYPASS_GYRO:   //  
+					 
+					  if( if(I2C_WRITE(GYRO_ADDRESS, Gyro_Calib_data[7],2)))   //  GYRO_ADDRESS,0x37,0x02
+					  {
+							st_Gyro=START_MAG;  // swith to next case after 10 ms
+					  }
+					 break;
+				case START_MAG:
+					  if( if(I2C_WRITE(MAG_ADDRESS, Mag_Calib_data[0],2)))   //  GYRO_ADDRESS,0x37,0x02
+					  {
+							st_Gyro=READ_MAG; 
+					  }
+					 break; //MAG_ADDRESS,0x0A,0x01
+				case READ_MAG: 
+					if(I2C_READ(MAG_ADDRESS, Mag_Read_Data[0],6))  // read all gyro values in Mag_Read_Data 
+					{
+							st_Gyro=CALC_MAG;
+					}
+					break;
+				case CALC_MAG: 
+					if(stI2CMgr==IDLE)	             
+					   {
+							InBuffer[1]=(Mag_Read_Data[1]<<8)|Mag_Read_Data[0];   // byte seq is differant for magneto
+							InBuffer[0]=(Mag_Read_Data[3]<<8)|Mag_Read_Data[2];
+							InBuffer[2]=-((Mag_Read_Data[5]<<8)|Mag_Read_Data[4]);
+					
+							 for(i = 0; i < 3; i ++)	
+							{
+								MPU9250_CalAvgValue(&MPU9250_Filter[i].Index, MPU9250_Filter[i].AvgBuffer, InBuffer[i], OutBuffer + i);
+							} 
+							magn[0] = *(OutBuffer + 0)-MPU9250_Magn_Offset.X_Off_Err;
+							magn[1] = *(OutBuffer + 1)-MPU9250_Magn_Offset.Y_Off_Err;
+							magn[2] = *(OutBuffer + 2)-MPU9250_Magn_Offset.Z_Off_Err;
+							st_Read=READ_ALT;
+						}
+					  break;
+			   case READ_ALT: 
+					if(I2C_READ(MAG_ADDRESS, Mag_Read_Data[0],6))  // read all gyro values in Mag_Read_Data 
+					{
+							st_Gyro=CALC_MAG;
+					}
+					break;
+				case CALC_MAG: 
+					if(stI2CMgr==IDLE)	             
+					   {
+							InBuffer[1]=(Mag_Read_Data[1]<<8)|Mag_Read_Data[0];   // byte seq is differant for magneto
+							InBuffer[0]=(Mag_Read_Data[3]<<8)|Mag_Read_Data[2];
+							InBuffer[2]=-((Mag_Read_Data[5]<<8)|Mag_Read_Data[4]);
+					
+							 for(i = 0; i < 3; i ++)	
+							{
+								MPU9250_CalAvgValue(&MPU9250_Filter[i].Index, MPU9250_Filter[i].AvgBuffer, InBuffer[i], OutBuffer + i);
+							} 
+							magn[0] = *(OutBuffer + 0)-MPU9250_Magn_Offset.X_Off_Err;
+							magn[1] = *(OutBuffer + 1)-MPU9250_Magn_Offset.Y_Off_Err;
+							magn[2] = *(OutBuffer + 2)-MPU9250_Magn_Offset.Z_Off_Err;
+							st_Read=READ_ALT;
+						}
+					  break;
+			}
+				
+						
+}
+
+bmp180_init
+{
+_oss = MODE_ULTRA_HIGHRES;
+}
+
 uint8 I2C_MasterReadBuf(uint8 slaveAddress, uint8 * rdData, uint8 cnt, uint8 mode);
 /* [] END OF FILE */
